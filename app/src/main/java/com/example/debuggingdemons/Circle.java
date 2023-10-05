b34064a6118b5e6fc5abea117d9ab5ce4c84a24d
Kele778 <100812664+Kele778@users.noreply.github.com>
2023-10-05 17:12:28 -0600

    Update and rename Circle.java to Triangle.java

--- app/src/main/java/com/example/debuggingdemons/Circle.java
+++
@@ -1,7 +0,0 @@
-package com.example.debuggingdemons;
-
-public class Circle extends Shape{
-    public Circle(int x, int y) {
-        super(x, y);
-    }
-}
---
+++ app/src/main/java/com/example/debuggingdemons/Triangle.java
@@ -0,0 +1,9 @@
+package com.example.debuggingdemons;
+
+public class Triangle extends Shape{
+    private int z;
+    public Triangle(int x, int y, int z) {
+        super(x, y);
+        this.z = z;
+    }
+}
