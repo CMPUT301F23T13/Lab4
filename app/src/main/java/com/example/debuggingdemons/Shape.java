7dfb096feab42df82d4f70e975c0b9c8ecfed854
Kele778 <100812664+Kele778@users.noreply.github.com>
2023-10-04 22:45:39 -0600

    Added color line in Shape.java

--- app/src/main/java/com/example/debuggingdemons/Shape.java
+++ app/src/main/java/com/example/debuggingdemons/Shape.java
@@ -3,6 +3,7 @@ package com.example.debuggingdemons;
 public class Shape {
     private int x;
     private int y;
+    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 
     public Shape(int x, int y) {
         this.x = x;
