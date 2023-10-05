26c32f2e3841b6609aa9be006ff0ab6e96d875b4
elialex15 <112982807+elialex15@users.noreply.github.com>
2023-10-05 16:02:54 -0600

    Deleted my circle class from main branch

--- app/src/main/java/com/example/debuggingdemons/Circle.java
+++
@@ -1,9 +0,0 @@
-package com.example.debuggingdemons;
-
-public class Circle extends Shape {
-    private int radius;
-    public Circle(int x, int y, int radius) {
-        super(x, y);
-        this.radius = radius;
-    }
-}
