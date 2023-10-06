d25871ba57cc80088bb86dc9c40aa003b4bbc35e
Peter Yu <yupeter666@yahoo.ca>
2023-10-06 12:36:03 -0600

    create rectangle model class

---
+++ app/src/main/java/com/example/debuggingdemons/Rectangle.java
@@ -0,0 +1,12 @@
+package com.example.debuggingdemons;
+
+public class Rectangle extends Shape {
+    private int length, width;
+
+    public Rectangle(int x, int y, int length, int width) {
+        super(x, y);
+        this.length = length;
+        this.width = width;
+    }
+
+}
