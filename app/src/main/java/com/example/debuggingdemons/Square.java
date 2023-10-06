32c83d0479b43f8d53f82e3664a0bc9481d4457a
Iris-Ling <140344004+Iris-Ling@users.noreply.github.com>
2023-10-07 04:44:40 +0800

    Update Square.java

--- app/src/main/java/com/example/debuggingdemons/Square.java
+++ app/src/main/java/com/example/debuggingdemons/Square.java
@@ -2,7 +2,7 @@ package com.example.debuggingdemons;
 
 public class Square extends Shape {
     private int sideLength;
-    private String color = "blue";
+ 
 
     public Square(int x, int y, int sideLength) {
         super(x,y);
